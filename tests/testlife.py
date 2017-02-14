5019f295bdc535a58e3848b5a1ed07a4aababddf
randomic <j.a.d.mcmillan@gmail.com>
2017-02-14 11:28:03 +0000

    Initial commit

---
+++ tests/testlife.py
@@ -0,0 +1,11 @@
+from gameoflife.Life import Life
+import unittest
+
+class testLife(unittest.TestCase):
+	def test_no_interaction(self):
+		emptyState = [  [0,0,0],
+				[0,0,0],
+				[0,0,0]  ]
+		testLife = Life()
+		self.assertEqual(emptyState, testLife.evolve(emptyState))
+
