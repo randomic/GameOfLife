22eac42032c81e32bad2578fcff20ffc227aac27
randomic <j.a.d.mcmillan@gmail.com>
2017-02-14 16:05:11 +0000

    Implement wrapping tests and solution
    Additionally fill out docstrings.

--- gameoflife/world.py
+++ gameoflife/world.py
@@ -1,70 +1,95 @@
+"""world module for Conway's Game of Life exercise.
+
+This module contains the `World` class which describes a 2 dimensional array
+of cells in either a living or dead state. The cells follow the rules of
+Conway's Game of Life. The `World` can be initialised with wrap_boundries=True
+to create a 'closed' world in which the bottom wraps to the top and the right
+wraps to the left.
+
+Example:
+    Initialise a 3x3 world which contains a period 2 blinker and evolve it
+    n_steps times::
+
+        >>> n_steps = 3
+        >>> initial_state = init_state = [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ]
+        >>> world = World(initial_state)
+        >>> world.simulate(n_steps)
+        >>> print(world)
+        [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+
+"""
 import copy
 
 
 class World(object):
+    """2 Dimensional world for Conway's Game of Life.
+
+    Stores the state of the `World` in a `list` which will be updated whenever
+    the `evolve` method is called.
+
+    Attributes:
+        logger: Logger named after this module.
+        size (`tuple`): Contains the size of the `World` as (rows, columns).
+        state (`list` of `list`): Contains the current state of the `World`.
+        wrap (`bool`): Whether or not the edges of the `World` 'wrap around'.
+
+    """
     def __init__(self, initial_state, wrap_boundaries=False):
-        self.size = (0, 0)
-        self.state = self.validate_state(initial_state)
+        """Initialise the world from `initial_state`.
+
+        Take the initial_state argument and validate it,  while also setting
+        the `size` attribute.
+
+        Args:
+            initial_state (`list` of `list`): Describing the initial state of
+                the `World`.
+            wrap_boundaries (`bool`): If `True` bottom of the `World` will wrap
+                to the top and the right edge will wrap to the left. If `False`
+                the `World` will act as though it has a border of permanently
+                'dead' cells.
+
+        """
+        self.state = copy.deepcopy(initial_state)
+        self.size = (len(self.state), len(self.state[0]))
         self.wrap = wrap_boundaries
 
-    def validate_state(self, state):
-        '''
-        Ensure the state is not missing information (i.e. irregular shape)
-        and determine the dimensions of the World.
-        Return a regularly shaped state.
-        '''
-        self.size = (len(state), len(state[0]))
-        return state
+    def simulate(self, n_steps):
+        """Evolve the world n_steps times and return the final state.
 
-    def count_neighbours(self, row, column):
-        '''
-        Return the number of living neighbours the cell at
-        (row, column) has.
-        '''
-        region = self.get_region(row, column)
-        n_count = 0
-        for r_row in region:
-            for cell in r_row:
-                n_count += cell
+        Returns:
+            The current state after evolving n_steps times.
 
-        n_count -= self.state[row][column]  # Do not count original cell
-        return n_count
+        """
+        for dummy_i in range(n_steps):
+            self.evolve()
 
-    def get_region(self, row, column):
-        '''
-        Return a 3x3 list of lists containing the cell at row, column
-        and its 8 adjacent cells.
-        '''
-        region = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]
-        for row_offset in range(-1, 2):
-            for column_offset in range(-1, 2):
-                n_row = row + row_offset
-                n_column = column + column_offset
-                if self.wrap:
-                    if n_row == self.size[0]:
-                        n_row = 0
-                    elif n_column == self.size[1]:
-                        n_column = 0
-                else:
-                    if (n_row in (-1, self.size[0]) or
-                            n_column in (-1, self.size[1])):
-                        region[row_offset + 1][column_offset + 1] = 0
-                        continue
-                region[row_offset + 1][column_offset + 1] = int(
-                    self.state[n_row][n_column]
-                )
-        return region
+        return self.state
 
     def evolve(self):
+        """Evaluate the new state from the current one and then update.
+
+        Returns:
+            The current state after being evolved and updated.
+
+        """
         new_state = self.step_cells()
         self.state = new_state
         return self.state
 
     def step_cells(self):
+        """Apply the rules of Game of Life to each cell in the `World`.
+
+        Note:
+            Only returns the new state, does not update `state`.
+
+        Returns:
+            The next state of the simulation after applying the rules.
+
+        """
         new_state = copy.deepcopy(self.state)
 
         for row in range(self.size[0]):
@@ -78,3 +103,59 @@ class World(object):
                     new_state[row][column] = 1
 
         return new_state
+
+    def count_neighbours(self, row, column):
+        """Return the number of living neighbours to (row, column).
+
+        Args:
+            row: The index of the row in which the cell of interest lies.
+            column: The index of the column in which the cell of interest lies.
+
+        Returns:
+            The total number of living cells out of the 8 adjacent cells.
+
+        """
+        region = self.get_region(row, column)
+        n_count = 0
+        for r_row in region:
+            for cell in r_row:
+                n_count += cell
+
+        n_count -= self.state[row][column]  # Do not count original cell
+        return n_count
+
+    def get_region(self, row, column):
+        """Return a 3x3 subsection of the world centred at (row, column).
+
+        Args:
+            row: The index of the row in which the centre cell lies.
+            column: The index of the column in which the centre cell lies.
+
+        Returns:
+            A 3x3 list of lists containing the centre cell and its 8 adjacent
+            cells.
+
+        """
+        region = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        for row_offset in range(-1, 2):
+            for column_offset in range(-1, 2):
+                n_row = row + row_offset
+                n_column = column + column_offset
+                if self.wrap:
+                    if n_row == self.size[0]:
+                        n_row = 0
+                    if n_column == self.size[1]:
+                        n_column = 0
+                else:
+                    if (n_row in (-1, self.size[0]) or
+                            n_column in (-1, self.size[1])):
+                        region[row_offset + 1][column_offset + 1] = 0
+                        continue
+                region[row_offset + 1][column_offset + 1] = int(
+                    self.state[n_row][n_column]
+                )
+        return region
---
+++ tests/testworld.py
@@ -0,0 +1,161 @@
+"""Test module for gameoflife.world in Conway's Game of Life exercise.
+
+This module contains test cases for an implementation of Conway's Game of Life.
+
+Example:
+    Run with nosetests::
+
+        $ nosetests -v
+
+"""
+import unittest
+
+from gameoflife.world import World
+
+
+class TestWorld(unittest.TestCase):
+    """Test unit for gameoflife.world World class"""
+    def test_no_interaction(self):
+        """Empty state should remain empty after evolving."""
+        empty_state = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        test_world = World(empty_state)
+        self.assertEqual(empty_state, test_world.evolve())
+
+    def test_underpopulation(self):
+        """Cells should 'die' to underpopulation after evolving."""
+        init_state = [
+            [0, 0, 1],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]
+        expected_state = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        test_world = World(init_state)
+        self.assertEqual(expected_state, test_world.evolve())
+
+    def test_overcrowding(self):
+        """Centre cell should 'die' to overcrowding after evolving."""
+        init_state = [
+            [0, 1, 1],
+            [0, 1, 0],
+            [1, 1, 0]
+        ]
+        expected_state = [
+            [0, 1, 1],
+            [0, 0, 0],
+            [1, 1, 0]
+        ]
+        test_world = World(init_state)
+        self.assertEqual(expected_state, test_world.evolve())
+
+    def test_survival(self):
+        """State should remain the same after evolving."""
+        init_state = [
+            [0, 1, 1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
+        expected_state = [
+            [0, 1, 1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
+        test_world = World(init_state)
+        self.assertEqual(expected_state, test_world.evolve())
+
+    def test_creation(self):
+        """Centre cell should 'live' after evolving."""
+        init_state = [
+            [0, 1, 1],
+            [0, 0, 1],
+            [0, 0, 0]
+        ]
+        expected_state = [
+            [0, 1, 1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
+        test_world = World(init_state)
+        self.assertEqual(expected_state, test_world.evolve())
+
+    def test_blinker(self):
+        """Initial state should oscillate in two evolutions."""
+        init_state = [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ]
+        evolved_state = [
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 1, 0]
+        ]
+        test_world = World(init_state)
+        self.assertEqual(evolved_state, test_world.evolve())
+        self.assertEqual(init_state, test_world.evolve())
+
+    def test_simulate(self):
+        """Oscillator should return initial state after evolving 15 times."""
+        init_state = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        test_world = World(init_state)
+        self.assertEqual(init_state, test_world.simulate(15))
+
+    def test_wrap(self):
+        """Oscillator should return initial state after evolving 15 times.
+
+        Note:
+            Same oscillator as `test_simulate` but offset into a corner and
+            `World` boundary wrapping switched on.
+
+        """
+        init_state = [
+            [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+        test_world = World(init_state, True)
+        self.assertEqual(init_state, test_world.simulate(15))
