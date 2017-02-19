89d255476a829c898936c000a46f6b402b37c116
randomic <j.a.d.mcmillan@gmail.com>
2017-02-19 22:01:03 +0000

    Add docstrings

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""Demonstration module for Conways Game of Life exercise.
+
+This module contains a single function `main` which intialises a `World` from
+the gameoflife.world module. The initial state of the world is a Gosper Glider
+Gun. matplotlib is used to make and plot an animation of the `World` with each
+frame calling the evolve method of the `World`.
+
+"""
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -5,6 +13,14 @@ from gameoflife.world import World
 
 
 def main():
+    """Animate a game of life world which contains a Gosper Glider Gun.
+
+    Notes:
+        Uses a matplotlib animation to repeatadly evolve the world and display
+        each iteration.
+
+    """
+
     world = World([
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -33,10 +49,22 @@ def main():
     img = plt.imshow(world.state)
 
     def update(data):
+        """Update img from frame data
+
+        Returns:
+            The updated img
+
+        """
         img.set_data(data)
         return img
 
     def data_gen():
+        """Generate frame data
+
+        Yields:
+            The evolved world state.
+
+        """
         while True:
             yield world.evolve()
 
